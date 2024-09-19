74c78d0a6fe50e48065a5cf8d0cfeb4d503d5e4c
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-19 14:15:18 +0700

    feat: set form style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
-import './App.css';
-import FormComponent from './components/Form.Component';
+import FormComponent from './components/FormComponent';
 
 function App() {
   return (
--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,17 +1,26 @@
+import './FormComponent.css'
+
 const FormComponent = () => {
   return (
     <div className="container">
       <form className="form">
+        <h2>Register Form</h2>
         <div className="form-control">
           <label>Username :</label>
           <input type="text" />
           <small>error message</small>
+        </div>
+        <div className="form-control">
           <label>Email :</label>
           <input type="text" />
           <small>error message</small>
+        </div>
+        <div className="form-control">
           <label>Password :</label>
           <input type="text" />
           <small>error message</small>
+        </div>
+        <div className="form-control">
           <label>Confirm password :</label>
           <input type="text" />
           <small>error message</small>
